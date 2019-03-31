94a10d421381856538ed046d93bab8d33ce13780
= <=>
2019-03-31 16:39:55 -0700

    added declarativeContent listener

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
+chrome.runtime.onInstalled.addListener(function() {
+  console.log('Installed');
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'web.groupme.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
+
 chrome.runtime.onMessage.addListener(
   function(message, sender, response) {
     console.log('received');
